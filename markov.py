47faf4dcf42152f358976c38db6eee675006a8e6
Mitchell Smith <zephyrthenoble@gmail.com>
2014-04-13 16:23:52 -0400

    Sentences work well, but capitalization is not kept

--- markov.py
+++ markov.py
@@ -33,7 +33,7 @@ def get_tokens(complete_file):
         #add it
         builder = builder + c
         #if it is a quote, handle options
-        if c == '"':
+        if c == '"' or c =="'":
             #if it was preceded by a checked sentence-ender, end sentence
             if sp:
                 sentences.append(process_string(builder))
@@ -70,43 +70,45 @@ tokens = []
 with open(read_file, 'r') as f:
     text = f.read()
     tokens = get_tokens(text)
-pprint(tokens)
 
+#pprint(tokens)
+#sys.exit(0)
+start_tokens = []
 m = defaultdict(list)
 for token in tokens:
-    t = token .split()
+    t = token.split()
     if len(t) < 2: continue
     first = t[0]
     second = t[1]
+    start_tokens.append(first+' '+second)
     for index in range(2, len(t)):
         word = t[index]
         while word.endswith('.') or word.endswith('?') or word.endswith('!'):
             if len(word) < 2:
                 break
-            print word
-            word = word[-1].strip()
+            #prt(word)
+            word = word[:-1].strip()
         m[first +" "+ second].append(word)
         #prt(first+second+" "+token[index])
         first = second
         second = t[index]
     m[first + " " + second].append('\n')
 
-for key, value in sorted(m.items()):
-    print key, sorted(value)
-    pass
+#for key, value in sorted(m.items()):
+#    print key, sorted(value)
+#    pass
 
+#sys.exit(0)
 temp = m
 
 x = 0
-constructed = choice(m.keys())
+constructed = choice(start_tokens)
 first, second = constructed.split()
 while '\n' not in constructed and len(constructed) < 200:
     if len(temp[first + ' ' + second]) <=0:
         break
     nextvalue = choice(temp[first+' '+second])
     del temp[first+' '+second]
-    if nextvalue == 'i':
-        nextvalue = 'i'.capitalize()
     if nextvalue.strip() == '.' or nextvalue.strip() == '?' or nextvalue.strip() == '!':
         continue
     constructed = constructed + ' ' + nextvalue
@@ -115,31 +117,4 @@ while '\n' not in constructed and len(constructed) < 200:
 
 constructed = constructed.capitalize().replace('\n','').strip()+'.'
 print constructed
-sys.exit(0)
-
-m = defaultdict(list)
-for token in tokens:
-    if len(token) > 1:
-        first = token[0]
-        second = token[1]
-        for index in range(2, len(token)):
-            m[first + second].append(token[index])
-            #prt(first+second+" "+token[index])
-            first = second
-            second = token[index]
-        m[first + second].append('\0')
-
-for key, value in sorted(m.items()):
-    print key, sorted(value)
-    pass
-
-
-print 
-
-for x in range (0, 4):
-    seed = choice(m.keys())
-    output = seed
-    while "\0" not in output and len(output) < 30:
-        output= output + choice(m[seed])
-    print output
 
