6fc337f398c9a498201de76b6cb9a66b7b047f8c
Mitchell Smith <zephyrthenoble@gmail.com>
2014-04-26 00:31:48 -0400

    Modified run

--- run.py
+++ run.py
@@ -12,6 +12,22 @@ def main():
     gen = Generator.load("dictionaries/peterpan.pickle")
     #get a sentence
     print (gen.generate_sentence())
+
+    count = 0
+    val = gen.generate_sentence()
+    #for x in range(0, 1000):
+    #    print gen.generate_sentence()
+    #sys.exit(0)
+    while val != "julia":
+        count+=1
+        if count % 1000 == 0:
+            print count
+            print val
+        val = gen.generate_sentence()
+        #sys.stdout.write("\r%s %d" %(,count))
+
+    print
+
     sys.exit(0)
 
 if __name__ == "__main__":
