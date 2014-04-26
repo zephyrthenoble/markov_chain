52538468199243dad9084d1fe4af86f9be74f3d6
Mitchell Smith <zephyrthenoble@gmail.com>
2014-04-26 00:29:54 -0400

    Changed how get in State works in order to use first order matching initially for words

--- markov/state.py
+++ markov/state.py
@@ -7,19 +7,29 @@ class State:
         self.constructed = ""
     def add(self, stritem):
         if stritem == "\n":
-            self.done = True
+            if len(self) >= 3:
+                self.done = True
+            else:
+                return
         self.constructed += " " + stritem
         self.state.append(stritem)
         self.state.pop(0)
     def get(self, num):
         index = -1*num
-        return " ".join(self.state[index:])
+        cons = ""
+        for elem in self.state[index:]:
+            if elem != None:
+                cons+=" "+ elem
+        cons = cons.strip()
+        return cons
     def reset(self):
         self.state = [None for i in range(0, self.length)]
     def finished(self):
         return self.done
     def get_str(self):
         return self.constructed
+    def get_word(self):
+        return self.constructed.replace(" ", "")
     def __len__(self):
         count = 0
         for elem in self.state:
