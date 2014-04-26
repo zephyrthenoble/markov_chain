21e9f8cb2da2a6d807fa418eec1aa2b45fa192e6
Mitchell Smith <zephyrthenoble@gmail.com>
2014-04-26 19:41:46 -0400

    changes to write

--- write.py
+++ write.py
@@ -4,18 +4,23 @@ from markov.functions import create_matching
 from markov import generator
 from random import choice
 DEBUG = False
+text = False
 
 if len(sys.argv) == 2 and sys.argv[1] == "debug":
     DEBUG = True
+if len(sys.argv) == 2 and sys.argv[1] == "text":
+    text = True
 
 def main():
-    gen = generator.word_generator()
-    #gen.read_folder("corpus")
-    #gen.read("corpus/shortpeterpan.txt")
-    #gen.read("corpus/peterpan.txt")
-    gen.read("corpus/passwords.txt")
-    gen.perturb_tokens(.01)
-    gen.create_ordered_paths()
+    if text:
+        gen = generator.prose_generator()
+        gen.read("corpus/shortpeterpan.txt")
+        gen.perturb_tokens(.01)
+        gen.create_ordered_paths()
+    else:
+        gen = generator.word_generator()
+        gen.read("corpus/passwords.txt")
+        gen.perturb_tokens(.01)
     gen.save("dictionaries/peterpan.pickle")
     sys.exit(0)
 
