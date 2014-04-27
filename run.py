e7fd23a32343c2e9340a06183bee90c98f91d6d7
Mitchell Smith <zephyrthenoble@gmail.com>
2014-04-27 13:27:37 -0400

    Testing

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import sys, getopt, pickle, logging
+import sys, getopt, pickle, logging, time
 from markov.functions import create_matching
 from markov.generator import Generator
 from random import choice
@@ -31,13 +31,23 @@ def main():
         val = gen.generate_word()
         maxcount = 26**len(test)
         count_index = maxcount / 100
-        print test, maxcount, count_index, count_index/100
-        with open("output/"+test+".txt", "w") as f:
+
+
+        tlist = []
+        plist = []
+        print "Looking for", test,"out of", maxcount, "combinations (26^"+str(len(test))+")"
+        for x in xrange(1000):
+            print "Test",x
+
+        #with open("output/"+test+".txt", "w") as f:
+            t0 = time.time()
+            val = gen.generate_word()
             while val != test:
                 count+=1
                 #print count
                 if count % (count_index/100) == 0:
-                    sys.stdout.write("\r%f%%" %((float)(count)/(float)(count_index)))
+                    #sys.stdout.write("\r%f%%" %((float)(count)/(float)(count_index)))
+                    sys.stdout.write("\r%f%%" %((float)(count)/(float)(maxcount)))
                     sys.stdout.flush()
                   #  print val
                 val = gen.generate_word()
@@ -45,9 +55,13 @@ def main():
                 #if len(sentences) >= 1024:
                 #    f.write("\n".join(sentences))
                 #    sentences = []
-
-        print
-        print count
+            t1 = time.time()
+            tlist.append(t1-t0)
+            plist.append((float)(count)/(float)(maxcount))
+            print
+            print count
+        print "Average time:\t\t\t", sum(tlist)/len(tlist)
+        print "Average percent searched:\t", sum(plist)/len(tlist)
 
 
     sys.exit(0)
