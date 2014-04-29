6d683818564309be56ede88ee15d6ef0b4de62ce
Zeatron <jqw3ha@virginia.edu>
2014-04-28 22:50:38 -0400

    user input for text

--- write.py
+++ write.py
@@ -3,26 +3,21 @@ from markov.functions import create_matching
 from markov import generator
 from random import choice
 DEBUG = False
-text = False
 
 if len(sys.argv) == 2 and sys.argv[1] == "debug":
     DEBUG = True
-if len(sys.argv) == 2 and sys.argv[1] == "text":
-    text = True
 
 def main():
-    if text:
-        gen = generator.prose_generator()
-        gen.read("corpus/shortpeterpan.txt")
-        gen.perturb_tokens(.01)
-        gen.create_ordered_paths()
-        gen.save("dictionaries/peterpan.pickle")
-    else:
-        gen = generator.word_generator()
-        gen.read("corpus/passwords.txt")
-        gen.perturb_tokens(.01)
-        gen.save("dictionaries/passwords.pickle")
-    sys.exit(0)
+	text = raw_input("Enter text: ")
+	if text == "":
+		text = "shortpeterpan.txt"
+
+    	gen = generator.prose_generator()
+    	gen.read("corpus/" + text)
+    	gen.perturb_tokens(.01)
+    	gen.create_ordered_paths()
+	gen.save("dictionaries/" + text[:-4] + ".pickle")
+    	sys.exit(0)
 
 if __name__ == "__main__":
     main()
